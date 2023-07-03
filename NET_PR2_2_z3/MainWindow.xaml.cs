eadd85e537306839df04fee2844ee739b816f4c3
Damord1 <89472598+Damord1@users.noreply.github.com>
2023-07-03 23:22:55 +0200

    done

---
+++ NET_PR2_2_z3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace NET_PR2_2_z3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+	ListBox lista;
+        public MainWindow()
+        {
+            InitializeComponent();
+            lista = (ListBox)FindName("Kategorie");
+        }
+
+        private void WidokWybranejKategorii(object sender, RoutedEventArgs e)
+        {
+            XmlElement wybrana = (XmlElement)lista.SelectedItem;
+            new Window2(wybrana).Show();
+        }
+
+        private void Kategorie_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
+
---
+++ NET_PR2_2_z3/Window2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace NET_PR2_2_z3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        ListBox lista;
+        public Window2(XmlElement kategoria)
+        {
+            DataContext = kategoria;
+            InitializeComponent();
+            lista = (ListBox)FindName("Podkategorie");
+        }
+        private void WidokWięcej(object sender, RoutedEventArgs e)
+        {
+
+            XmlElement wybrana = (XmlElement)lista.SelectedItem;
+            new Window1(wybrana)
+                .Show();
+
+        }
+    }
+}
